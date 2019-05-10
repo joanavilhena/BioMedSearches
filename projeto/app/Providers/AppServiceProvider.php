9ce1988914e033c80accdfe5e3a611885443910c
Joana Vilhena <2131107@my.ipleiria.pt>
2019-05-10 19:03:39 +0100

    test

--- projeto/app/Providers/AppServiceProvider.php
+++ projeto/app/Providers/AppServiceProvider.php
@@ -27,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        $url = 'biomedsearch.me';
         if(env('REDIRECT_HTTPS')) {
             $url->formatScheme('https');
         }
